ef5e0cf33524dbc5fc765282c8384a46afc8afc0
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-07 13:24:32 +0100

    3d modlle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
     const [active, setActive] = useState('');
+    const [toggle, setToggle] = useState('');
 
 
     return (
@@ -17,15 +18,27 @@ const Navbar = () => {
                 <Link to='/'
                     className='flex items-center gap-2' onClick={() => { setActive(''); window.scrollTo(0, 0) }}>
                     <img src={logo} alt="logo" className=' w-9 h-9 object-contain' />
-                    <p className='text-white text-[18px] font-bold cursor-pointer'>Mehdi <span className='sm:block hidden'>|  Ridmani</span></p>
+                    <p className='text-white text-[18px] flex font-bold cursor-pointer'>Mehdi &nbsp;<span className='sm:block hidden'>|&nbsp;Ridmani</span></p>
                 </Link>
-                <ul className='list-non hidden sm:flex flex row gap-10'>
+                <ul className='list-non hidden sm:flex  row gap-10'>
                     {navLinks.map((link) => (
                         <li key={link.id} className={`${active === link.title ? 'text-white' : 'text-secondary'} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
                             <a href={`#${link.id}`}>{link.title}</a>
                         </li>
                     ))}
                 </ul>
+                <div className=' sm:hidden flex flex-1 justify-end items-center'>
+                    <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
+                    <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 max-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                        <ul className='list-non flex justify-end items-start flex-col gap-4'>
+                            {navLinks.map((link) => (
+                                <li key={link.id} className={`${active === link.title ? 'text-white' : 'text-secondary'} font-poppins font-medium text-[16px] cursor-pointer`} onClick={() => { setActive(link.title); setToggle(!toggle) }}>
+                                    <a href={`#${link.id}`}>{link.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     )
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import { CanvasLoader } from '../Loader';
 
 const Computers = () => {
+    const computer = useGLTF('./desktop_pc/scene.gltf');
     return (
-        <div>Computers</div>
+        <mesh>
+            <hemisphereLight intensity={0.15} groundColor='black' />
+            <pintlight intensity={1} />
+            <primitive object={computer.scene} />
+        </mesh>
+    )
+}
+
+const ComputersCanvas = () => {
+    return (
+        <Canvas>
+
+        </Canvas>
     )
 }
 
