3e2662fdb46312523c8d45c49ffe992fe9680d93
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-08 12:57:58 +0100

    3d modell

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
@@ -18,6 +18,19 @@ const Hero = () => {
                 </div>
             </div>
             <ComputersCanvas />
+            <div className=' absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+                <a href="#about">
+                    <div className=' w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                        <motion.dev animate={{
+                            y: [0, 24, 0]
+                        }}
+                            transition={{
+                                duration: 1.5, repeat: Infinity, repeatType: 'loop'
+                            }}
+                            className="w-3 h-3 rounded-full  bg-secondary mb-1" />
+                    </div>
+                </a>
+            </div>
         </section>
 
     )
