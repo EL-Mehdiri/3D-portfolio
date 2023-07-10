c0a95e805a5f7c63bd63500bcedf2092099acd02
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-10 19:18:08 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Navbar, Tech, Works, StarsCanvas } from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
   return (
@@ -7,6 +7,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
+          <Hero />
         </div>
         <About />
         <Experience />
