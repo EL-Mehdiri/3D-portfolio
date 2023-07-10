6f9514053e1ddd44b76f2e9dd7842cf3d73e29fc
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-10 11:42:16 +0100

    stars canvas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
                 <p className={styles.sectionSubText}>Get in toutch</p>
                 <h3 className={styles.sectionHeadText}>Contact</h3>
 
-                <form action="" ref={formRef} onClick={handleSubmit} className="mt-12 flex flex-col gap-8">
+                <form ref={formRef} onClick={handleSubmit} className="mt-12 flex flex-col gap-8">
                     <label className='flex flex-col'>
                         <span className='text-white font-medium mb-4'>Your Name</span>
                         <input
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
+
+
+
+
+const Stars = (props) => {
+    const ref = useRef();
+
+    const sphare = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+
+    useFrame((state, deLta) => {
+        ref.current.rotation.x -= deLta / 10;
+        ref.current.rotation.y *= deLta / 15;
+    })
+
     return (
-        <div>Stars</div>
+        <group rotation={[0, 0, Math.PI / 4]}>
+            <Points ref={ref} positions={sphare} stride={3} frustumCulled {...props} >
+                <PointMaterial transparent color='#f272c8' size={0.002} sizeAttenuation={true} depthWrite={false} />
+            </Points>
+        </group >
+    )
+}
+const StarsCanvas = () => {
+    return (
+        <div className=" w-full h-auto absolute inset-0 z-[-1]">
+            <Canvas camera={{ position: [0, 0, 1] }}>
+                <Suspense fullback={null}>
+                    <Stars />
+                </Suspense>
+                <Preload all />
+            </Canvas>
+        </div>
     )
 }
 
-export default Stars
+export default StarsCanvas;
