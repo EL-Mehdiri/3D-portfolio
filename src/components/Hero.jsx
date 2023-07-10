e31bf7a360295e5c2d59cfe25c87aea6b87cc720
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-10 19:27:04 +0100

    n

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-// import { ComputersCanvas } from './canvas';
+import { ComputersCanvas } from './canvas';
 const Hero = () => {
     return (
         <section className='relative w-full h-screen mx-auto'>
@@ -17,7 +17,7 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
-            {/* <ComputersCanvas /> */}
+            <ComputersCanvas />
             <div className=' absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
                 <a href="#about">
                     <div className=' w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
