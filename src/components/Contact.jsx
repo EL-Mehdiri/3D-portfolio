3fbcf837d5d4cff7febf67e77955a7527a2eedd8
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-10 12:17:10 +0100

    done!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,39 +1,79 @@
-import { useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-
 const Contact = () => {
     const formRef = useRef();
     const [form, setForm] = useState({
-        name: '',
-        email: '',
-        message: '',
+        name: "",
+        email: "",
+        message: "",
     });
+
     const [loading, setLoading] = useState(false);
 
     const handleChange = (e) => {
+        const { target } = e;
+        const { name, value } = target;
 
-    }
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    };
+
     const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
 
-    }
+        emailjs.send('service_e62fqeo', 'template_tob92bq',
+            {
+                form_name: form.name, to_name: 'mehdi',
+                from_email: form.email, to_email: 'mehdiridmani@gmail.com',
+                message: form.message
+            },
+            'YNH56uFJSGvsD9Fr_')
+            .then(
+                () => {
+                    setLoading(false);
+                    alert("Thank you. I will get back to you as soon as possible.");
 
+                    setForm({
+                        name: "",
+                        email: "",
+                        message: "",
+                    });
+                },
+                (error) => {
+                    setLoading(false);
+                    console.error(error);
 
-
+                    alert("Ahh, something went wrong. Please try again.");
+                }
+            );
+    };
 
     return (
-        <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-            <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
-                <p className={styles.sectionSubText}>Get in toutch</p>
-                <h3 className={styles.sectionHeadText}>Contact</h3>
+        <div
+            className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+        >
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+            >
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-                <form ref={formRef} onClick={handleSubmit} className="mt-12 flex flex-col gap-8">
+                <form
+                    ref={formRef}
+                    onSubmit={handleSubmit}
+                    className='mt-12 flex flex-col gap-8'
+                >
                     <label className='flex flex-col'>
                         <span className='text-white font-medium mb-4'>Your Name</span>
                         <input
@@ -76,6 +116,7 @@ const Contact = () => {
                     </button>
                 </form>
             </motion.div>
+
             <motion.div
                 variants={slideIn("right", "tween", 0.2, 1)}
                 className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
@@ -83,7 +124,7 @@ const Contact = () => {
                 <EarthCanvas />
             </motion.div>
         </div>
-    )
-}
+    );
+};
 
-export default SectionWrapper(Contact, 'contact')
+export default SectionWrapper(Contact, "contact");
